38638715113eab03fb2e93bbeba836944435c466
Maryelen <maryelenw@gmail.com>
2017-09-07 14:35:49 -0300

    Professor - atributos

--- src/Entity/Professor.java
+++ src/Entity/Professor.java
@@ -12,5 +12,33 @@ package Entity;
 public class Professor {
     
     String nome;
+    int matricula;
+    Disciplina disciplina;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+    
+    
     
 }
