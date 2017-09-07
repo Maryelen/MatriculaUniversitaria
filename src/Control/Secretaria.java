d5de6e7a4c50f498dd2e561c2ca6b0015511b1d0
Maryelen <maryelenw@gmail.com>
2017-09-07 14:48:50 -0300

    Secretaria  atributos

--- src/Control/Secretaria.java
+++ src/Control/Secretaria.java
@@ -5,10 +5,36 @@
  */
 package Control;
 
+import Entity.Aluno;
+import Entity.Professor;
+import java.util.ArrayList;
+
 /**
  *
  * @author Maryelen
  */
 public class Secretaria {
+    
+    ArrayList<Professor> listaProfessor;
+    ArrayList<Aluno> listaAluno;
+
+    public ArrayList<Professor> getListaProfessor() {
+        return listaProfessor;
+    }
+
+    public void setListaProfessor(ArrayList<Professor> listaProfessor) {
+        this.listaProfessor = listaProfessor;
+    }
+
+    public ArrayList<Aluno> getListaAluno() {
+        return listaAluno;
+    }
+
+    public void setListaAluno(ArrayList<Aluno> listaAluno) {
+        this.listaAluno = listaAluno;
+    }
+    
+    
+    
     
 }
