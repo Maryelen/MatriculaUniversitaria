9fbc575793a1dbef1c718e1e42fd75d53a078046
Maryelen <maryelenw@gmail.com>
2017-09-07 14:33:08 -0300

    Aluno adicionando os atributos

--- src/Entity/Aluno.java
+++ src/Entity/Aluno.java
@@ -11,7 +11,33 @@ package Entity;
  */
 public class Aluno {
     
-    String nome;
-    int idade;	
-int matricula;
+    String nome;	
+    int matricula;
+    Disciplina disciplina;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+    
+    
 }
