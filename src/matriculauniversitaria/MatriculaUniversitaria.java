263956b4fc54f6d3426790c7218a61b503f8f21f
ariane <ariane@192.168.15.7>
2017-09-07 12:40:11 -0300

    Teste de commit

--- src/matriculauniversitaria/MatriculaUniversitaria.java
+++ src/matriculauniversitaria/MatriculaUniversitaria.java
@@ -16,6 +16,8 @@ public class MatriculaUniversitaria {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String teste = "teste";
     }
     
 }
