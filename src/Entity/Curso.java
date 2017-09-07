4643c24997eb43d64d892d0fb2112cc31c5281f5
Maryelen <maryelenw@gmail.com>
2017-09-07 14:40:31 -0300

    Arrumando o View, classe Curso

--- src/Entity/Curso.java
+++ src/Entity/Curso.java
@@ -5,10 +5,50 @@
  */
 package Entity;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Maryelen
  */
 public class Curso {
+    
+    String nome;
+    String unidade;
+    Disciplina disciplina;
+    ArrayList<Disciplina> listaDisciplinasCurso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public ArrayList<Disciplina> getListaDisciplinasCurso() {
+        return listaDisciplinasCurso;
+    }
+
+    public void setListaDisciplinasCurso(ArrayList<Disciplina> listaDisciplinasCurso) {
+        this.listaDisciplinasCurso = listaDisciplinasCurso;
+    }
+    
     
 }
