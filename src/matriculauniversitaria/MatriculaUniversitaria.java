368887c471e7231b9ba2936fb505f97c23f42eb5
Maryelen <maryelenw@gmail.com>
2017-09-07 14:16:00 -0300

    Revert "Pacotes"
    This reverts commit c9817b45fff0f07fbbe4727258283af173d76dee.

--- src/matriculauniversitaria/MatriculaUniversitaria.java
+++ src/matriculauniversitaria/MatriculaUniversitaria.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -22,28 +22,3 @@ public class MatriculaUniversitaria {
     }
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package matriculauniversitaria;
-
-/**
- *
- * @author Maryelen
- */
-public class MatriculaUniversitaria {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        String teste = "teste2";
-    }
-    
-}
->>>>>>> 82a8e34cc3da6afd8daaf933b592a841a3eb081b
